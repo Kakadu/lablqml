fb60a7879e2097a4457b47f44efc3a2f042ac8c5
kakadu <kakadu.hafanana@gmail.com>
2011-08-19 21:50:29 +0400

    xml file updated

--- test_gen/qtstubs.ml
+++ test_gen/qtstubs.ml
@@ -2,6 +2,13 @@ type 'a obj
 
 let (|>) x f = f x 
 
+class virtual ['a] sssignal = object 
+  method virtual name : string
+end
+class virtual ['a, 'c] ssslot = object 
+  method virtual name : string 
+  method virtual call : 'c 
+end
 
 external make_root_widget : unit -> [`qobject] obj = "make_root_widget"
 
