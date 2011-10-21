59fed020a72332f95aaf6e3cec55a195d755ac81
kakadu <kakadu.hafanana@gmail.com>
2011-10-22 00:16:47 +0400

    fixed test for new api

--- test_gen/test/main.ml
+++ test_gen/test/main.ml
@@ -2,15 +2,27 @@ open Qtstubs
 open Stub_helpers
 open Classes
 open Stubs
+open Creators
 
-let app = QApplication.create [| "1";"2";"3";"4";"5";"6" |] 
+let app = QApplication.create [| "1";"2";"3";"4";"5";"6" |];; 
  (* HACK to avoid bug with optimization levels *)
 
+print_endline "creating normal QObject";;
+let qobj = create_QObject_0 None;;
+print_endline "normal qobject created";;
 
-let root_widget = make_root_widget () |> new qWidget 
-let () = print_endline "root_widget created"
+class myqObject me = object (self)
+  inherit qObject me as super
+  method! setObjectName name = 
+          Printf.printf "setting name of myqObject to %s\n" name;
+          flush stdout;
+          super#setObjectName name
+end;;
 
-let _ = root_widget#slot_show#call 
+print_endline "creating my qObject";;
+let myqobj = create_QObject_0' None |> new myqObject;;
+print_endline "setting name of my qobject";;
+let () = myqobj#setObjectName "somename";;
 let _ = QApplication.exec app
 
 
