a871793c40b066d7f977b7020e65196924b020ea
kakadu <kakadu.hafanana@gmail.com>
2011-11-29 15:50:52 +0400

    adoptiong for crash representing

--- for_test5.sh
+++ for_test5.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-lst="QDialog QMenuBar QMenu QAction QGroupBox QHBoxLayout QGridLayout QLabel QLineEdit QTextEdit QFormLayout QComboBox QLabel QSpinBox QHBoxLayout QVBoxLayout QPushButton QDialogButtonBox Qt QAbstractSlider QScrollBar QTabWidget QCheckBox QListWidget QTreeWidget"
+lst="QSpinBox Qt"
 
 ./xml/xmltool.native -in aaa.xml -out for_test5.xml $lst 
