1deef2cb99daba8ee8e8636e718a31e7560db149
kakadu <kakadu.hafanana@gmail.com>
2011-12-03 17:15:43 +0400

    fixed build

--- xml/ocamlGenerator.ml
+++ xml/ocamlGenerator.ml
@@ -411,8 +411,8 @@ class ocamlGenerator graph dir index = object (self)
   method makefile dir = 
     ignore (Sys.command ("touch " ^ dir ^/ ".depend"));
     let h = open_out (dir ^ "/Makefile") in
-    fprintf h "ML_MODULES=stubs.cmo creators.cmo classes.cmo \n";
-    fprintf h "ML_MODULES_OPT=stubs.cmx creators.cmx classes.cmx  \n\n";
+    fprintf h "ML_MODULES=stubs.cmo classes.cmo creators.cmo\n";
+    fprintf h "ML_MODULES_OPT=stubs.cmx classes.cmx creators.cmx\n\n";
     fprintf h "OCAMLC=ocamlc.opt -g\nOCAMLOPT=ocamlopt.opt -g\n\n";
     fprintf h "INC=-I ./../../test_gen \n";
     fprintf h ".SUFFIXES: .ml .mli .cmi .cmx .cmo \n\n";
@@ -420,7 +420,7 @@ class ocamlGenerator graph dir index = object (self)
     fprintf h ".ml.cmx:\n\t$(OCAMLOPT) $(INC) -c $<\n\n";
     fprintf h ".mli.cmi:\n\t$(OCAMLC)  $(INC) -c $<\n\n";
     fprintf h "all: byte opt\n\n";
-    fprintf h "depend:\n\tocamldep $(INC) *.ml *.mli > .depend\n\n";
+(*    fprintf h "depend:\n\tocamldep $(INC) *.ml *.mli > .depend\n\n";*)
     fprintf h "byte: $(ML_MODULES)\n\n";
     fprintf h "opt:  $(ML_MODULES_OPT)\n\n";
     fprintf h ".PHONY: all clean opt byte\n\n";
