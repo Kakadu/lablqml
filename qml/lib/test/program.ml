3e7e2f6ba7104ef03de50f01fc8cf59ef4045e9a
kakadu <kakadu.hafanana@gmail.com>
2013-03-16 16:18:59 +0400

    Example fixed

--- qml/lib/test/program.ml
+++ qml/lib/test/program.ml
@@ -1,35 +1,66 @@
 open QmlContext
+external (|>) : 'a -> ('a -> 'b) -> 'b = "%revapply"
 
 let main () =
-  print_endline "Do Something";
-  let data = ["A";"B";"C";"D"] in
-  let open A in
-  let (model: typ_for_A) = object(self)
-    method sizey _ = 0
+  print_endline "Do something";
+  let data = [["A"];["B";"C"]; ["D";"E";"F"]]
+    |> List.map (List.map (fun str ->
+      let open DataItem in
+      let (item: typ_for_DataItem) = object
+        method name () = str
+        method sort () = String.lowercase str
+      end in
+      create_DataItem item
+    ))
+    |> List.map (fun data ->
+      let open MiniModel in
+      let (model: typ_for_MiniModel) = object(self)
+        method parent _ = QModelIndex.empty
+        method index row column parent =
+          if (row>=0 && row<self#rowCount parent) then QModelIndex.make ~row ~column:0
+          else QModelIndex.empty
+        method columnCount _ = 1
+        method rowCount _ = List.length data
+        method hasChildren _ = self#rowCount QModelIndex.empty > 0
+        method data index role =
+          let r = QModelIndex.row index in
+          if (r<0 || r>= List.length data) then QVariant.empty
+          else begin
+            if (role=0 || role=555) (* DisplayRole *)
+            then QVariant.of_object (List.nth data r)
+            else QVariant.empty
+          end
+      end in
+      let cpp_model = create_MiniModel model in
+      add_role cpp_model 555 "qwe";
+      cpp_model
+    ) in
+
+  let open MainModel in
+  let (model: typ_for_MainModel) = object(self)
     method parent _ = QModelIndex.empty
     method index row column parent =
       if (row>=0 && row<self#rowCount parent) then QModelIndex.make ~row ~column:0
       else QModelIndex.empty
-    method columnCount _ =
-      print_endline "columnCount";
-      1
-    method rowCount _ : int =
-      List.length data
-
+    method columnCount _ = 1
+    method rowCount _ = List.length data
     method hasChildren _ = self#rowCount QModelIndex.empty > 0
     method data index role =
-      print_endline "data"; flush stdout;
       let r = QModelIndex.row index in
       if (r<0 || r>= List.length data) then QVariant.empty
       else begin
         if (role=0 || role=555) (* DisplayRole *)
-        then QVariant.of_string (List.nth data r)
+        then QVariant.of_object (List.nth data r)
+        (*then QVariant.of_string "XXX"*)
         else QVariant.empty
       end
 
   end in
-  let cpp_model = create_A model in
+  print_endline "1";
+  let cpp_model = create_MainModel model in
+  print_endline "2";
   add_role cpp_model 555 "homm";
+  print_endline "3";
   set_context_property ~ctx:(get_view_exn ~name:"rootContext") ~name:"myModel" cpp_model
 ;;
 let () = Callback.register "doCaml" main
--- qml/lib/test/view.qml
+++ qml/lib/test/view.qml
@@ -1,20 +1,54 @@
 import QtQuick 2.0
 
 Rectangle {
-    color: "green"
-
+    id: root
+    width: 800
+    height: 600
+    color: "white"
     ListView {
-        width: 200
-        height: 250
-
         model: myModel
-        delegate: Text {
-            text: "Animal: " + model.homm
-            //Component.onCompleted: console.log("text completed, " + homm + ", " + model.homm);
-        }
-        Component.onCompleted: {
-            console.log("roleNames = " + model.roles() );
-            console.log("ListView completed, " + model);
+
+        width: parent.width
+        orientation: ListView.Horizontal
+
+        delegate: Rectangle {
+            width: 250
+            color: "lightgray"
+            anchors.rightMargin: 5
+            anchors.leftMargin: 5
+            ListView {
+                id: lv1
+                ScrollBar {
+                    flickable: lv1
+                    vertical: true
+                    hideScrollBarsWhenStopped: false
+                    scrollbarWidth: 5
+                }
+
+                model: homm
+                spacing: 5
+                width: parent.width
+                height: 250
+                orientation: ListView.Vertical
+                delegate:
+                    Rectangle {
+                        radius: 5
+                        anchors.rightMargin: 15
+                        anchors.leftMargin: 5
+                        width: lv1.width - 15
+                        x: 5
+                        height: 35
+                        color: "black"
+                        Text { text: qwe.name() + " " + qwe.sort()
+                            anchors.fill: parent
+                            color: "white"
+                            font {
+                                family: "Consolas"
+                                pixelSize: 30
+                            }
+                        }
+                }
+            }
         }
 
     }
