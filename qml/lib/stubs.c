457a4eee8579df10093c1a2f141e31ac2fc91222
kakadu <kakadu.hafanana@gmail.com>
2013-03-30 14:30:17 +0400

    Code of lib fixed

--- qml/lib/stubs.c
+++ qml/lib/stubs.c
@@ -1,25 +1,21 @@
 #include "stubs.h"
 
-
 void registerContext(const QString& name, QQmlContext* v) {
-  //qDebug() << "void registerView(....)";
-  //CAMLparam0();
+  CAMLparam0();
+  CAMLlocal3(_name,_view,_ans);
   static value *closure = nullptr;
   if (closure == nullptr) {
     closure = caml_named_value("register_view");
   }
   Q_ASSERT(closure!=nullptr);
-  static value _name;
-  static value _view;
   _name = caml_copy_string(name.toStdString().c_str());
   _view = caml_alloc_small(1, Abstract_tag);
   (*((QQmlContext **) &Field(_view, 0))) = v;
-  static value _ans;
   _ans = caml_callback2(*closure, _name, _view); // should be a unit
   Q_UNUSED(_ans);
-  //CAMLreturn0;
+  CAMLreturn0;
 }
-// ctx:t -> name:string -> 'a  -> unit
+// ctx:t -> name:string -> cppobj -> unit
 extern "C" value caml_setContextProperty(value _ctx, value _name, value _cppObj) {
   CAMLparam3(_ctx,_name,_cppObj);
 
@@ -36,7 +32,8 @@ extern "C" value caml_setContextProperty(value _ctx, value _name, value _cppObj)
 extern "C" value caml_set_caml_object(value _cppobj,value _camlobj) {
   CAMLparam2(_cppobj, _camlobj);
   QObject *o = ((QObject*) Field(_cppobj,0));
-  //TODO: register_global_root
+
+  //caml_register_global_root(&_camlobj);
   o->setProperty(CAMLOBJ_PROPERTY, QVariant::fromValue(_camlobj) );
   CAMLreturn(Val_unit);
 }
