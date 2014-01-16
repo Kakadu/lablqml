1bffe025f8ac3f9c5c2ad41bc39bc2f03030466c
kakadu <kakadu.hafanana@gmail.com>
2014-01-16 14:53:00 +0000

    Added options -destdir and -ext
    New options for ocamlbuild integration:
      -ext c          forces tool go generate source files with .c extension
      -ext cpp        will generate .cpp files (default)
      -destdir <path> sets output directory where to put generated files;
                      "." is default value

--- src/mocml/mocml.ml
+++ src/mocml/mocml.ml
@@ -11,6 +11,8 @@ type options = {
   mutable target : [ `Qml | `QtGui | `Qml_wrap ];
   mutable add_debug_calls: bool;
   mutable debug_critical_sections: bool;
+  mutable default_extension : [ `C | `CPP ];   (* Which extension to use for generated files *)
+  mutable dest_dir : string;            (* Where to put generated files *)
 }
 
 let options =
@@ -18,6 +20,8 @@ let options =
   ; target = `Qml_wrap
   ; add_debug_calls = false
   ; debug_critical_sections = false
+  ; default_extension = `CPP
+  ; dest_dir = "."
   }
 
 let () = Core_arg.parse
@@ -28,6 +32,12 @@ let () = Core_arg.parse
          "Add qDebug() calls in beginning of member functions")
   ; ("-with-debug-criticals", Arg.Unit (fun () -> options.debug_critical_sections <- true),
          "Add qDebug() when using critical setions")
+  ; ("-destdir",    Arg.String (fun s -> options.dest_dir <- s), "Where to put generated files")
+  ; ("-ext",        Arg.String (function | "c"   -> options.default_extension <- `C
+					 | "cpp" -> options.default_extension <- `CPP
+					 | ____  -> failwith "Unknown parameter of -ext"),
+     "set extension for generated files")
+
   ; ("-help",       Arg.Unit (fun () -> ignore (Sys.command "man mocml")), "help")
   ; ("-h",          Arg.Unit (fun () -> ignore (Sys.command "man mocml")), "help")
   ] (fun s -> options.filename <- s;
@@ -36,15 +46,15 @@ let () = Core_arg.parse
 
 let () = match options.target with
   | `QtGui -> begin
-    let funcs = ParseYaml.parse options.filename in
-    let () = print_endline ("slots parsed: " ^ string_of_int (List.length funcs)) in
-    let slots = List.map funcs ~f:(fun (name,args) ->
-      let args = Core_list.map args ~f:(fun x -> `Simple x) in
-      (* FIXME check for lists too *)
-      let res = Core_list.last_exn args in
-      let body = Core_list.rev args |> Core_list.tl_exn in
-      (name,body,res)
-    ) in
+      let funcs = ParseYaml.parse options.filename in
+      let () = print_endline ("slots parsed: " ^ string_of_int (List.length funcs)) in
+      let slots = List.map funcs ~f:(fun (name,args) ->
+          let args = Core_list.map args ~f:(fun x -> `Simple x) in
+          (* FIXME check for lists too *)
+          let res = Core_list.last_exn args in
+          let body = Core_list.rev args |> Core_list.tl_exn in
+          (name,body,res)
+      ) in
     (*
     let clas =
       let open Parse.Yaml2.Types in
@@ -86,8 +96,9 @@ let () = match options.target with
         exit 1
     in
     let config =
+      [`Ext options.default_extension] @
       (if options.add_debug_calls then [`PrintMethCalls] else []) @
       (if options.debug_critical_sections then [`DebugBlockingSections] else [])
     in
-    List.iter data ~f:(Qml_wrap.generate ~config)
+    List.iter data ~f:(Qml_wrap.generate ~directory:options.dest_dir ~config)
   end
