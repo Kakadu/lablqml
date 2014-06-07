de94c7740511c2f865aae71ff05892bea0527271
kakadu <kakadu.hafanana@gmail.com>
2014-06-07 19:27:51 +0400

    generate some additional members

--- ppx_test/lib/ppx_qt.ml
+++ ppx_test/lib/ppx_qt.ml
@@ -4,6 +4,7 @@ open Asttypes
 open Parsetree
 open Longident
 open Location
+open Printf
 
 module Ref = struct
   let append ~set x = set := x :: !set
@@ -32,9 +33,9 @@ let (_: Ast_mapper.mapper) = default_mapper
 let cppobj_coretyp loc =
   Ast_helper.Typ.constr ~loc ({txt=Lident "cppobj"; loc}) []
 
-let make_store_func ~loc =
+let make_store_func ~loc ~classname : structure_item =
   let pval_name = {txt="store"; loc} in
-  let pval_prim = ["name_of_external_func_there"] in
+  let pval_prim = [sprintf "caml_store_value_in_%s" classname] in
   let pval_type = Ast_helper.Typ.(arrow ""
                                         (cppobj_coretyp loc)
                                         (arrow ""
@@ -45,9 +46,27 @@ let make_store_func ~loc =
                                   pval_loc=loc } in
   { pstr_desc; pstr_loc=loc }
 
+let make_initializer ~loc : class_field =
+  let pexp_desc = Ast_helper.Exp.(
+    apply (ident (mkloc (Lident "store") loc))
+          [ ("",ident (mkloc (Lident "cppobj") loc))
+          ; ("",ident (mkloc (Lident "self") loc))
+          ]
+          )
+  in (* TODO: *)
+  let pcf_desc = Pcf_initializer pexp_desc in
+  { pcf_desc; pcf_loc=loc; pcf_attributes=[] }
+
+let make_handler_meth ~loc : class_field =
+  let e = Ast_helper.Exp.(poly (ident (mkloc (Lident "cppobj") loc) ) None) in
+  Ast_helper.(Cf.method_ (mkloc "handler" loc) Public (Cfk_concrete (Fresh,e)  ) )
+
+let wrap_meth ~classname (({txt=name;_},_,kind) as m) = [Pcf_method m]
+let wrap_prop ~classname (({txt=name;_},_,kind) as v) = [Pcf_method v]
+
 let wrap_class_decl mapper loc _ (ci: Parsetree.class_expr Parsetree.class_infos) =
   (*print_endline "wrap_class_type_decl on class type markend with `qtclass`";*)
-
+  let classname = ci.pci_name.txt in
   let clas_sig = match ci.pci_expr.pcl_desc with
     | Pcl_structure s -> s
     |  _    -> raise (Error2 ("???", ci.pci_loc))
@@ -55,23 +74,26 @@ let wrap_class_decl mapper loc _ (ci: Parsetree.class_expr Parsetree.class_infos
   let fields: class_field list = clas_sig.pcstr_fields in
 
   let heading = ref [] in
-  Ref.append ~set:heading (make_store_func loc);
-  let wrap_meth ((name,_,typ) as m) = [Pcf_method m] in
-  let wrap_prop ((name,_,typ) as v) = [Pcf_method v] in
+  Ref.append ~set:heading (make_store_func   ~classname ~loc);
 
   let wrap_field (f_desc: class_field) : class_field list =
     let ans = match f_desc.pcf_desc with
-      | Pcf_method m  when has_attr "qtmeth" f_desc.pcf_attributes -> wrap_meth m
-      | Pcf_method m  when has_attr "qtprop" f_desc.pcf_attributes -> wrap_prop m
+      | Pcf_method m  when has_attr "qtmeth" f_desc.pcf_attributes -> wrap_meth ~classname m
+      | Pcf_method m  when has_attr "qtprop" f_desc.pcf_attributes -> wrap_prop ~classname m
       | _ -> []
     in
     let ans' = List.map (fun ans -> { f_desc with pcf_desc=ans }) ans in
     ans'
   in
   let new_fields = List.map wrap_field fields |> List.concat in
-  let new_desc = Pcl_structure {clas_sig with pcstr_fields = new_fields} in
+  let new_fields = (make_initializer loc) :: (make_handler_meth ~loc) :: new_fields in
+  let new_desc = Pcl_structure { pcstr_fields = new_fields;
+                                 pcstr_self = Ast_helper.Pat.var (mkloc "self" loc) } in
   let new_expr = {ci.pci_expr with pcl_desc = new_desc } in
-  let ans = { pstr_desc=Pstr_class [{ci with pci_expr=new_expr}]; pstr_loc=loc } in
+  let new_desc2 = Pcl_fun ("", None, Ast_helper.Pat.var (mkloc "cppobj" loc), new_expr) in
+  let new_expr2 = {new_expr with pcl_desc = new_desc2 } in
+
+  let ans = { pstr_desc=Pstr_class [{ci with pci_expr=new_expr2}]; pstr_loc=loc } in
   !heading @ [ans]
 
 (*
--- ppx_test/test_qt/def.ml
+++ ppx_test/test_qt/def.ml
@@ -1,12 +1,13 @@
 (*
 external store: cppobj -> < .. > -> unit = "caml_store_value_in_Controller"
  *)
-class virtual controller = object
+class virtual controller = object(self)
   method virtual hasData : bool[@@qtprop]
   (*val name: string*)
   method virtual linkActivated : string -> unit[@@qtmeth]
         (*
   method backTo : string -> int -> unit
   method getDefaultLibraryPath: unit -> string *)
+
 end[@@qtclass]
 
