42daa1fcce3f9f4524911c7c4e97d99a0da8eb86
kakadu <kakadu.hafanana@gmail.com>
2015-11-08 15:28:55 +0300

    Fixed compilation

---
+++ qml/helloworld/controller.ml
@@ -0,0 +1,8 @@
+open QmlContext
+
+class virtual controller = object(self)
+
+  method virtual onMouseClicked: string  -> unit[@@qtmeth]
+
+
+end[@@qtclass]
--- qml/helloworld/program.ml
+++ qml/helloworld/program.ml
@@ -1,9 +1,9 @@
 open QmlContext
 
 let main () =
-  let controller_cppobj = Controller.create_Controller () in
+  let controller_cppobj = Controller.create_controller () in
   let controller = object(self)
-    inherit Controller.base_Controller controller_cppobj as super
+    inherit Controller.controller controller_cppobj as super
     method onMouseClicked msg = Printf.printf "OCaml says: '%s'\n%!" msg
   end in
   set_context_property ~ctx:(get_view_exn ~name:"rootContext") ~name:"controller" controller#handler;
