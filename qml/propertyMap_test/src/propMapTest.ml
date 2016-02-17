3067cde905c4be1d20241408cbcc888ae1e57473
kakadu <kakadu.hafanana@gmail.com>
2016-02-17 17:57:33 +0400

    Added threads to demostrate that changing values from OCaml works

--- qml/propertyMap_test/src/propMapTest.ml
+++ qml/propertyMap_test/src/propMapTest.ml
@@ -24,8 +24,19 @@ end
 let main () =
   let map = PropMap.create () in
   PropMap.insert map ~name:"title" (QVariant.of_string "hello");
-  PropMap.insert map ~name:"count" (QVariant.of_int 11);
+  PropMap.insert map ~name:"count" (QVariant.of_int 0);
   set_context_property ~ctx:(get_view_exn ~name:"rootContext") ~name:"propMap1" (PropMap.handler map);
+
+  let (_: Thread.t) =
+    let counter = ref 0 in
+    let rec f () =
+      let _ = Thread.delay 1.0 in
+      incr counter;
+      PropMap.insert map ~name:"count" (QVariant.of_int !counter);
+      f ()
+    in
+    Thread.create f ()
+  in
   ()
 
 let _ =
