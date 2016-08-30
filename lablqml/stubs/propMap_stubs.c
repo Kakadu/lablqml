d672758d03a4f118e32b19c61250636e67e8b73a
SP <sp@orbitalfox.com>
2016-08-30 16:29:32 +0100

    Binding of QML properties to specified OCaml functions.
    Added API to get QObjects from QML using `objectName` property.
    Added API to extract from properties values of type QQmlProperty. They
      allow to read and write properties in the generic way.
    Added API to connect custom OCaml functions to properties. These
    functions behave the same as slots connected to onChange signal of
    specific property.
    Added demo about features above.
    
    Implementation by OrbitalFox. Review and squash by Kakadu.

--- lablqml/stubs/propMap_stubs.c
+++ lablqml/stubs/propMap_stubs.c
@@ -1,8 +1,9 @@
 #include "lablqml.h"
 
+#include "variant.h"
 #include "CamlPropertyMap.h"
-#include <QtCore/QDebug>
 
+#include <QtCore/QDebug>
 #include <QtQml/QQmlEngine>
 
 extern "C" {
@@ -12,7 +13,6 @@ extern "C" {
 #include <caml/alloc.h>
 #include <caml/callback.h>
 #include <caml/custom.h>
-
 
 /* Custom CamlPropertyMap block freeing operations */
 
@@ -31,62 +31,6 @@ static struct custom_operations camlpropertymap_ops = {
   custom_deserialize_default,
   custom_compare_ext_default
 };
-
-// converts QVariant to OCaml QVariant.t. Should be ported to the lablqml
-/*
-// I intentionally skipped CAMLlocalN because intuition says that they are
-// not needed because it is done in caller function
-*/
-value Val_QVariant(value _dest, const QVariant& var) {
-    CAMLparam1(_dest);
-    CAMLlocal1(_var);
-
-    if (!var.isValid()) {
-        _dest = hash_variant("empty");
-    } else {
-        const int ut = var.userType();
-
-        switch (ut) {
-        case QMetaType::Bool:
-            _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("bool"));
-            Store_field(_dest, 1, Val_bool(var.toBool()));
-            break;
-        case QMetaType::QString:
-            _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("string"));
-            Store_field(_dest, 1, caml_copy_string(var.value<QString>().toLocal8Bit().data()));
-            break;
-        case QMetaType::Int:
-            _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("int"));
-            Store_field(_dest, 1, Val_int(var.value<int>()));
-            break;
-        case QMetaType::Float:
-        case QMetaType::Double:
-            _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("float"));
-            Store_field(_dest, 1, caml_copy_double(var.toFloat()));
-            break;
-        case QMetaType::User:
-        case QMetaType::QObjectStar:
-            {
-              QObject *vvv = var.value<QObject*>();
-              _var = caml_alloc_small(1,Abstract_tag);
-              (*((QObject **) &Field(_var, 0))) = vvv;
-              _dest = caml_alloc(2,0);
-              Store_field(_dest, 0, hash_variant("qobject"));
-              Store_field(_dest, 1, _var);
-            }
-            break;
-        default:
-            QString msg("Type is not supported:");
-            msg += QString("userType() == %1").arg(ut);
-            Q_ASSERT_X(false, __func__, msg.toLocal8Bit().data() );
-        }
-    }
-    CAMLreturn(_dest);
-}
 
 value caml_create_QQmlPropertyMap(value _func, value _unit) {
     CAMLparam2(_func, _unit);
@@ -132,35 +76,12 @@ value caml_QQmlPropertyMap_value(value _map, value _propName) {
     CAMLreturn(_ans);
 }
 
-
 value caml_QQmlPropertyMap_insert(value _map, value _propName, value _variant) {
     CAMLparam3(_map, _propName, _variant);
 
-    // copy and paste from the generated file for QAbstractModel subclass
-    // TODO: move this conversion to the lablqml
-    QVariant newval;
-    if (Is_block(_variant)) {
-        if (caml_hash_variant("bool") == Field(_variant,0) )
-            // without cast it will create Qvariant of int
-            newval = QVariant::fromValue( (bool)Bool_val(Field(_variant,1)) );
-        else if (caml_hash_variant("string") == Field(_variant,0) )
-            newval = QVariant::fromValue(QString(String_val(Field(_variant,1))));
-        else if (caml_hash_variant("int") == Field(_variant,0) )
-            newval = QVariant::fromValue(Int_val(Field(_variant,1)));
-        else if (caml_hash_variant("float") == Field(_variant,0) )
-            newval = QVariant::fromValue(Double_val(Field(_variant,1)));
-        else if (caml_hash_variant("qobject") == Field(_variant,0) )
-            newval = QVariant::fromValue((QObject*) (Field(Field(_variant,1),0)));
-        else
-            Q_ASSERT_X(false, "While converting OCaml value to QVariant",
-                       "Unknown variant tag");
-    } else { // empty QVariant
-        newval = QVariant();
-    }
-
     CamlPropertyMap *map = (*(CamlPropertyMap**) (Data_custom_val(_map)));
     Q_ASSERT_X(map != NULL, __func__, "Trying to use QQmlPropertyMap object which is NULL");
-    map->insert( QString(String_val(_propName)), newval);
+    map->insert( QString(String_val(_propName)), QVariant_val(_variant) );
 
     CAMLreturn(Val_unit);
 }
--- lablqml/stubs/startup_stubs.c
+++ lablqml/stubs/startup_stubs.c
@@ -11,15 +11,6 @@
 #include <QtQuick/QQuickWindow>
 #include <QtQuick/QQuickItem>
 
-static value Val_some(value v) {
-  CAMLparam1(v);
-  CAMLlocal1(some);
-
-  some = caml_alloc(1, 0);
-  Store_field(some, 0, v);
-
-  CAMLreturn(some);
-}
 /* This is a crazy macro to allocation argc and argv and pass them to
  * QApplication. The problem is that we need copying of argv because they are
  * ocaml values that can be moved. Moreover, we need to allocate argc because
