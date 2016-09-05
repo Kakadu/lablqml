71c224a804b44b4d61698508c833d69c440967d9
Stavros Polymenis <sp@orbitalfox.com>
2016-09-05 20:02:36 +0100

    moved propMap to global context, or cppObj will get collected

--- demos/propmap_stress_test/main.ml
+++ demos/propmap_stress_test/main.ml
@@ -1,10 +1,17 @@
 open Lablqml
 
-  let value_changed name value = match value with
+let value_changed name value = match value with
     | `int i -> if i mod 1000 = 0 then (Printf.printf "%s %d;\n" name i; flush Pervasives.stdout;)
     | _ -> ()
-  in
-  let alpha = PropMap.create ~callback:value_changed () in
+
+let alpha = PropMap.create ~callback:value_changed ()
+let callback _ _ = ()
+let beta = PropMap.create ~callback ()
+
+let qml_mapper () =
+(*  let i_channel = Unix.in_channel_of_descr socket in
+  let o_channel = Unix.out_channel_of_descr socket in *)
+
   set_context_property ~ctx:(get_view_exn ~name:"rootContext") ~name:"alpha" (PropMap.handler alpha);
   PropMap.insert alpha ~name:"countA" (QVariant.of_int 0);
   PropMap.insert alpha ~name:"c0" (QVariant.of_int 0);
