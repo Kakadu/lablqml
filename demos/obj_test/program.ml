f7ab835913b36bd93741dfc2c1df9fef8f7011e6
kakadu <kakadu@pm.me>
2021-06-04 21:11:34 +0300

    Trying to fix compilation

--- demos/obj_test/program.ml
+++ demos/obj_test/program.ml
@@ -3,32 +3,43 @@ open Lablqml
 
 let () = Printexc.record_backtrace true
 
-class myitem cppObj = object(self)
-  inherit Item.item cppObj as super
-  method getname () = "Vasya"
-  method getage () = 11
-end
+class myitem cppObj =
+  object (self)
+    inherit Item.item cppObj as super
+
+    method getname () = "Vasya"
+
+    method getage () = 11
+  end
 
 let main () =
   let cppObj = Item.create_item () in
   let item = new myitem cppObj in
 
   let controller_cppobj = Controller.create_controller () in
-  let controller = object(self)
-    inherit Controller.controller controller_cppobj as super
-    method getobj () = QVariant.of_object item#handler
-    method getperson () = self#getobj ()
-    method setobj  = function
-      | `qobject o ->
-         let item = new myitem o in
-         printf "qobject: %s %d\n%!" (item#getname ()) (item#getage())
-      | `string s -> printf "String: '%s'\n%!" s
-      | `int  x -> printf "int %d\n%!" x
-      | `float f -> printf "float %f\n%!" f
-      | `bool b -> printf "bool %b\n%!" b
-      | `empty -> print_endline "empty"
-  end in
+  let controller =
+    object (self)
+      inherit Controller.controller controller_cppobj as super
 
-  set_context_property ~ctx:(get_view_exn ~name:"rootContext") ~name:"controller" controller#handler
+      method getobj () = QVariant.of_object item#handler
+
+      method getperson () = self#getobj ()
+
+      method setobj =
+        function
+        | `qobject o ->
+            let item = new myitem o.obj in
+            printf "qobject: %s %d\n%!" (item#getname ()) (item#getage ())
+        | `string s -> printf "String: '%s'\n%!" s
+        | `int x -> printf "int %d\n%!" x
+        | `float f -> printf "float %f\n%!" f
+        | `bool b -> printf "bool %b\n%!" b
+        | `empty -> print_endline "empty"
+    end
+  in
+
+  set_context_property
+    ~ctx:(get_view_exn ~name:"rootContext")
+    ~name:"controller" controller#handler
 
 let () = Callback.register "doCaml" main
