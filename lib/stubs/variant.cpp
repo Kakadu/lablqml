f07cbceaff0761f02fff8340f489eac8bed6d3f7
Damien Bihel <biheldamien@gmail.com>
2022-12-20 11:36:41 +0100

    fix: add OCaml 5 support
    Before OCaml5, hash_variant was defined as an alias for
    caml_hash_variant in compatibility.h file
    (#define hash_variant caml_hash_variant)
    
    Seems not present in OCaml5 so this patch rename hash_variant
    to caml_hash_variant.

--- lib/stubs/variant.cpp
+++ lib/stubs/variant.cpp
@@ -34,30 +34,30 @@ extern "C" {
     CAMLlocal1(_var);
 
     if (!var.isValid()) {
-        _dest = hash_variant("empty");
+        _dest =caml_hash_variant("empty");
     } else {
         const int ut = var.userType();
 
         switch (ut) {
         case QMetaType::Bool:
             _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("bool"));
+            Store_field(_dest, 0,caml_hash_variant("bool"));
             Store_field(_dest, 1, Val_bool(var.toBool()));
             break;
         case QMetaType::QString:
             _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("string"));
+            Store_field(_dest, 0,caml_hash_variant("string"));
             Store_field(_dest, 1, caml_copy_string(var.value<QString>().toLocal8Bit().data()));
             break;
         case QMetaType::Int:
             _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("int"));
+            Store_field(_dest, 0,caml_hash_variant("int"));
             Store_field(_dest, 1, Val_int(var.value<int>()));
             break;
         case QMetaType::Float:
         case QMetaType::Double:
             _dest = caml_alloc(2, 0);
-            Store_field(_dest, 0, hash_variant("float"));
+            Store_field(_dest, 0,caml_hash_variant("float"));
             Store_field(_dest, 1, caml_copy_double(var.toFloat()));
             break;
         case QMetaType::User:
@@ -67,7 +67,7 @@ extern "C" {
               _var = caml_alloc_small(1,Abstract_tag);
               (*((QObject **) &Field(_var, 0))) = vvv;
               _dest = caml_alloc(2,0);
-              Store_field(_dest, 0, hash_variant("qobject"));
+              Store_field(_dest, 0,caml_hash_variant("qobject"));
               Store_field(_dest, 1, _var);
             }
             break;
